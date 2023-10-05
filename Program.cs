33a1ecf46f442994979eab15c984b9859a65d8c3
parlaTharuni <tharuniparla@gmail.com>
2023-10-05 16:59:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace firstprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            Console.WriteLine("enter first number");
+            int firstno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            int secondno = Convert.ToInt32(Console.ReadLine());
+            int ans = firstno + secondno;
+            Console.WriteLine($"the answer of{firstno}+{secondno}={ans}");
+            Console.ReadLine();
+            //Console.WriteLine(  );
+           
+            
+        }
+    }
+}
